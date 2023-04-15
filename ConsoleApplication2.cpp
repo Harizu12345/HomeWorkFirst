87a04e10e31a09049e0da7b1e19ff07b44d6fe58
msi <msi@DESKTOP-Q39FBF1>
2023-04-15 23:54:02 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,110 @@
+﻿/*
+// home task 1 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   cout << "Monday  Tuesday  Wensday  Thursday  Friday  Saturday  Sunday\n";
+   cout << "  27      28       29       30        31       1         2\n";
+   cout << "  3        4        5        6         7       8         9\n";
+   cout << "  10      11       12       13        14      15        16\n";
+   cout << "  17      18       19       20        21      22        23\n";
+   cout << "  24      25       26       27        28      29        30\n";
+}
+*/
+
+
+/*
+// home task 2
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "         (\n";
+	cout << "            )\n";
+	cout << "         ( _   _._\n";
+	cout << "          |_|-'_~_`-._\n";
+	cout << "       _.-'-_~_-~_-~-_`-._\n";
+	cout << "   _.-'_~-_~-_-~-_~_~-_~-_`-._\n";
+	cout << "  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+	cout << "    |  []  []   []   []  [] |\n";
+	cout << "    |           __    ___   |   \n";
+	cout << "  ._|  []  []  | .|  [___]  |_._._._._._._._._._._._._._._._._.  \n";
+	cout << "  |=|________()|__|()_______|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=| \n";
+	cout << "^^^^^^^^^^^^^^^ === ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n";
+
+}
+*/
+
+
+
+/*
+// home task 3
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << " * * * *                          *** *    /        ************    *           *     ************                    \n";
+	cout << " *     *                       *      *             *               *           *     *          *            \n"; 
+	cout << " *      *                    *        *             *               *           *     *          *             \n";
+	cout << " *          *            *            *    *        *               *           *     *          *            \n";
+	cout << " *           *         *              *    *        *               *           *     *          *           \n";
+	cout << " *             *     *                *    *        ************    *************     ************                     \n";
+	cout << " *               * *                  *    *                   *    *           *     *          *                \n";
+	cout << " *                                    *    *                   *    *           *     *          *          \n";
+	cout << " *                                    *    *                   *    *           *     *          *           \n";
+	cout << " *                                    *    *                   *    *           *     *          *           \n";
+	cout << " *                                    *    *        ************    *           *     *          *         \n";
+
+}
+
+*/
+
+
+/*
+// home task 4
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "                                       *\n";
+	cout << "                                     *\n";
+	cout << "                                   *\n";
+	cout << "                             *\n";
+	cout << "                      *\n";
+	cout << " *\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "                       *\n";
+	cout << "*\n";
+}
+
+*/
+
+
+/*
+// home task 5
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "\tMisha\n";
+	cout << "\tYears 15\n";
+	cout << "\tSometimes play basketball\n";
+	cout << "\tright now i lern c++\n";
+	cout << "\tand i now little bit pyhton\n";
+}
+
+*/
